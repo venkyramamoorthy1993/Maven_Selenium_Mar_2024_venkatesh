aba6af9bc5152dd0fda6d6ecfd9c1b5d00aa933d
Venkatesh Raja G R <venkatesh.raja@freshworks.com>
2024-03-11 23:28:20 +0530

    commit data
    checking

--- src/main/java/week1/day1/BasicJavacode.java
+++ src/main/java/week1/day1/BasicJavacode.java
@@ -2,6 +2,6 @@ package week1.day1;
 
 public class BasicJavacode {
 	public static void main(String[] args) {
-		System.out.println("Welcome To Testleaf git hub checking");	
+		System.out.println("Welcome To Testleaf");	
 	}
 }
